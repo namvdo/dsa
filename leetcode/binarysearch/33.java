e775d4b7252118665202a2203b925f43deb5b2ac
Nam V. Do <nam.dv@coccoc.com>
2022-01-21 23:09:17 +0700

    Search in rotated sorted array

--- leetcode/binarysearch/33.java
+++ leetcode/binarysearch/33.java
@@ -1,18 +1,29 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length - 1;
-        while (low < high) {
-            int mid = (low + high) / 2;
-            if ((nums[mid] < nums[0]) == (target < nums[0])) {
-                if (nums[mid] < target) {
-                    low = mid + 1;
-                } else if (nums[mid] > target) {
-                    high = mid;
-                } else return mid;
-            } else if (target < nums[0]) low = mid + 1;
-            else high = mid;
-        } 
+        if (nums == null || nums.length == 0) return -1;
+
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) left = mid + 1;
+            else right = mid;
+        }
+
+        int start = left;
+        left = 0;
+        right = nums.length - 1;
+
+        if (target >= nums[start] && target <= nums[right]) left = start;
+        else right = start;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+
         return -1;
     }
 
