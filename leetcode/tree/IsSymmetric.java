a17c5853f347dc02bbad7eb3b74758c910c9f6fb
Nam Van Do <nam.dv@coccoc.com>
2023-07-01 17:52:51 +0700

    Add is symmetric tree

---
+++ leetcode/tree/IsSymmetric.java
@@ -0,0 +1,13 @@
+package leetcode.tree;
+
+public class IsSymmetric {
+   public boolean isSymmetric(TreeNode root) {
+       return dfs(root.left, root.right);
+    }
+    
+    boolean dfs(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        return (left.val == right.val && dfs(left.left, right.right) && dfs(left.right, right.left));
+    }   
+}
