a8d563659c35c991399292d74481c6f3996246dd
Nam Van Do <nam.dv@coccoc.com>
2023-07-26 16:50:43 +0700

    Add arrow shots

--- leetcode/interval/ArrowShots.java
+++ leetcode/interval/ArrowShots.java
@@ -5,22 +5,22 @@ import java.util.Comparator;
 
 public class ArrowShots {
     public int findMinArrowShots(int[][] points) {
+        if (points.length == 0) return 0;
         Arrays.sort(points, new Comparator<>() {
             public int compare(int[] a, int[] b) {
-                return Integer.compare(a[1], b[1]);
+                return Integer.compare(a[0], b[0]);
             }
         });
-        int arrowPos = points[0][1];
-        int arrowCnt = 1;
+        int[] prev = points[0];
+        int c = 1;
         for(int i = 1; i < points.length; i++) {
-            if (arrowPos >= points[i][0]) {
-                continue;
+            if (points[i][0] > prev[1]) {
+                c++;
+                prev = new int[] {points[i][0], points[i][1]};
+            } else {
+                prev[1] = Math.min(prev[1], points[i][1]);
             }
-            arrowCnt++;
-            arrowPos = points[i][1];
         }
-        return arrowCnt;
+        return c;
     }
-}
- 
 }
