9a76a9782897b4d4c21c2eaf4a49363c44541b8f
Nam Van Do <nam.dv@coccoc.com>
2023-07-29 18:11:42 +0700

    Add Max depth of binary tree

---
+++ leetcode/tree/MaxDepth.java
@@ -0,0 +1,9 @@
+package leetcode.tree;
+
+public class MaxDepth {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+        return 1 + (Math.max(maxDepth(root.left), maxDepth(root.right)));
+    }
+}
