e6f3407532945f624180c495b806d52c18f6f3a3
Nam Van Do <nam.dv@coccoc.com>
2023-07-29 18:33:31 +0700

    Update Is Symmetric Tree

--- leetcode/tree/IsSymmetric.java
+++ leetcode/tree/IsSymmetric.java
@@ -1,13 +1,17 @@
 package leetcode.tree;
 
 public class IsSymmetric {
-   public boolean isSymmetric(TreeNode root) {
-       return dfs(root.left, root.right);
+    public boolean isSymmetric(TreeNode root) {
+        return dfs(root.left, root.right);
     }
-    
+
     boolean dfs(TreeNode left, TreeNode right) {
+        boolean t = false;
         if (left == null && right == null) return true;
-        if (left == null || right == null) return false;
-        return (left.val == right.val && dfs(left.left, right.right) && dfs(left.right, right.left));
-    }   
+        else if ((left == null && right != null) || (left != null && right == null)) return false;
+        else {
+            t = left.val == right.val;
+        }
+        return t && dfs(left.left, right.right) && dfs(left.right, right.left);
+    }
 }
