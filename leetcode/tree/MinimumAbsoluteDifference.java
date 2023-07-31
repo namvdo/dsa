ede3f5394ade2ab1148ed02f17d9e664fdd9c015
Nam Van Do <nam.dv@coccoc.com>
2023-07-31 22:03:37 +0700

    Add minimum absolute difference

---
+++ leetcode/tree/MinimumAbsoluteDifference.java
@@ -0,0 +1,16 @@
+package leetcode.tree;
+
+public class MinimumAbsoluteDifference {
+   Integer prev = null;
+   int min = Integer.MAX_VALUE;
+   public int getMinimumDifference(TreeNode root)  {
+        if (root == null) return min;
+        getMinimumDifference(root.left);
+        if (prev != null) {
+            min = Math.min(min, root.val - prev);
+        }
+        prev = root.val;
+        getMinimumDifference(root.right);
+        return min;
+   }
+}
