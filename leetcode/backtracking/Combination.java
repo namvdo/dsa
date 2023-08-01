ca17355f6c79d6a8071bd277ba7cf086feefb429
Nam Van Do <nam.dv@coccoc.com>
2023-08-01 22:21:02 +0700

    Add combinations

---
+++ leetcode/backtracking/Combination.java
@@ -0,0 +1,24 @@
+package leetcode.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combination {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> combs = new ArrayList<>();
+        combine(combs, new ArrayList<>(), 1, n, k);
+        return combs;
+    }
+
+    void combine(List<List<Integer>> combs, List<Integer> comb, int start, int n, int k) {
+        if (k == 0) {
+            combs.add(new ArrayList<>(comb));
+            return;
+        }
+        for (int i = start; i <= n; i++) {
+            comb.add(i);
+            combine(combs, comb, i + 1, n, k - 1);
+            comb.remove(comb.size() - 1);
+        }
+    }
+}
